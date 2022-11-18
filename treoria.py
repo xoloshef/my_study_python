5a2230c6ac5f29dcb7511a3e6eacdef57b138339
Xoloshef <xoloshef@gmail.com>
2022-11-19 02:03:31 +0300

    study(0.4)

--- treoria.py
+++ treoria.py
@@ -1,28 +1,10 @@
-# тема 4.2
-# возврат значений из функции
+a, b = map(int, input().split())
 
-def p_plus(a, b):
-    print(a + b)
-
-def r_plus(a, b):
+def plus(a, b):
     return a + b
 
-p_sum = p_plus(2, 3) # тк мы не просим вернуть p_sum = None
-r_sum = r_plus(2, 3) # None 5
+def pr(a, b):
+    return a + b
 
-print(p_sum, r_sum)
-
-# Функция возвращает одно значение
-# работет def только до return
-
-
-# позиционное
-r_sum = r_plus(2, 3)
-# непозиционное
-
-
-
-
-
-
-
+print(plus(a, b))
+print(pr(a, b))
